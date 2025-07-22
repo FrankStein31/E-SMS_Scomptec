70efdc6c7ac14b7e854631ba7789967099312c62
Frankie Steinlie <frankie.intern24slides@gmail.com>
2025-07-22 10:07:29 +0700

    dashboard admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\MasterKlasifikasi;
+use App\Models\MasterInstansi;
+use App\Models\MasterTindakanDisposisi;
+use App\Models\EntrySuratIsi;
+use App\Models\SuratKeluarIsi;
 
 class DashboardController extends Controller
 {
@@ -22,6 +27,14 @@ class DashboardController extends Controller
             'total_users' => User::count(),
             'total_admin' => User::where('jabatan', 'Administrator')->count(),
             'total_user' => User::where('jabatan', '!=', 'Administrator')->count(),
+            'total_klasifikasi' => MasterKlasifikasi::count(),
+            'total_instansi' => MasterInstansi::count(),
+            'total_tindakan' => MasterTindakanDisposisi::count(),
+            'total_surat_masuk' => EntrySuratIsi::count(),
+            'total_surat_keluar' => SuratKeluarIsi::count(),
+            'total_surat_terkirim' => SuratKeluarIsi::where('status', '>=', 2)->count(),
+            'latest_users' => User::orderBy('created_at', 'desc')->take(5)->get(),
+            'latest_surat_keluar' => SuratKeluarIsi::orderBy('created_at', 'desc')->take(5)->get(),
         ];
         return view('dashboard.admin', $data);
     }
--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -15,7 +15,7 @@
         </div>
 
         <div class="row">
-            <div class="col-sm-6 col-xl-4 col-12">
+            <div class="col-sm-6 col-xl-3 col-12">
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex align-items-start justify-content-between">
@@ -30,8 +30,7 @@
                     </div>
                 </div>
             </div>
-
-            <div class="col-sm-6 col-xl-4 col-12">
+            <div class="col-sm-6 col-xl-3 col-12">
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex align-items-start justify-content-between">
@@ -46,8 +45,7 @@
                     </div>
                 </div>
             </div>
-
-            <div class="col-sm-6 col-xl-4 col-12">
+            <div class="col-sm-6 col-xl-3 col-12">
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex align-items-start justify-content-between">
@@ -59,6 +57,156 @@
                                 <i class="iconoir-user-circle f-s-22"></i>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3 col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex align-items-start justify-content-between">
+                            <div>
+                                <h5>Klasifikasi</h5>
+                                <h2 class="counter">{{ $total_klasifikasi ?? 0 }}</h2>
+                            </div>
+                            <div class="avatar-45 b-r-10 bg-info">
+                                <i class="iconoir-folder f-s-22"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3 col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex align-items-start justify-content-between">
+                            <div>
+                                <h5>Instansi</h5>
+                                <h2 class="counter">{{ $total_instansi ?? 0 }}</h2>
+                            </div>
+                            <div class="avatar-45 b-r-10 bg-secondary">
+                                <i class="iconoir-building f-s-22"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3 col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex align-items-start justify-content-between">
+                            <div>
+                                <h5>Tindakan Disposisi</h5>
+                                <h2 class="counter">{{ $total_tindakan ?? 0 }}</h2>
+                            </div>
+                            <div class="avatar-45 b-r-10 bg-danger">
+                                <i class="iconoir-check-circle f-s-22"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3 col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex align-items-start justify-content-between">
+                            <div>
+                                <h5>Surat Masuk</h5>
+                                <h2 class="counter">{{ $total_surat_masuk ?? 0 }}</h2>
+                            </div>
+                            <div class="avatar-45 b-r-10 bg-dark">
+                                <i class="iconoir-mail f-s-22"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3 col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex align-items-start justify-content-between">
+                            <div>
+                                <h5>Surat Keluar</h5>
+                                <h2 class="counter">{{ $total_surat_keluar ?? 0 }}</h2>
+                            </div>
+                            <div class="avatar-45 b-r-10 bg-primary">
+                                <i class="iconoir-send f-s-22"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3 col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex align-items-start justify-content-between">
+                            <div>
+                                <h5>Surat Terkirim</h5>
+                                <h2 class="counter">{{ $total_surat_terkirim ?? 0 }}</h2>
+                            </div>
+                            <div class="avatar-45 b-r-10 bg-success">
+                                <i class="iconoir-paper-plane f-s-22"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header bg-light">
+                        <b>User Terbaru</b>
+                    </div>
+                    <div class="card-body p-0">
+                        <table class="table table-sm mb-0">
+                            <thead>
+                                <tr>
+                                    <th>Username</th>
+                                    <th>Nama</th>
+                                    <th>Jabatan</th>
+                                    <th>Unit Kerja</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($latest_users ?? [] as $u)
+                                <tr>
+                                    <td>{{ $u->username }}</td>
+                                    <td>{{ $u->fullname }}</td>
+                                    <td>{{ $u->jabatan }}</td>
+                                    <td>{{ $u->satker->satker ?? '-' }}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header bg-light">
+                        <b>Surat Keluar Terbaru</b>
+                    </div>
+                    <div class="card-body p-0">
+                        <table class="table table-sm mb-0">
+                            <thead>
+                                <tr>
+                                    <th>No Surat</th>
+                                    <th>Perihal</th>
+                                    <th>Tanggal</th>
+                                    <th>Pembuat</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($latest_surat_keluar ?? [] as $s)
+                                <tr>
+                                    <td>{{ $s->nosurat }}</td>
+                                    <td>{{ $s->hal }}</td>
+                                    <td>{{ $s->tgl_surat }}</td>
+                                    <td>{{ $s->user->fullname ?? '-' }}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
