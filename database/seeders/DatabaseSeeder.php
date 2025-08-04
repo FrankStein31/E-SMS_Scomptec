1985a0b7f274d5acf227a90742a14c42b797b744
Frankie Steinlie <frankie.intern24slides@gmail.com>
2025-08-04 12:02:13 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,6 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             MasterSeeder::class,
             EntrySuratIsiSeeder::class,
-            MasterTindakanSeeder::class,
         ]);
     }
 }
